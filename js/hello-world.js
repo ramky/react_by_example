0a079ad074e1dd449eb68ff9f4ad8a5e58f5847d
Ram Iyer <ram.iyer@bydeluxe.com>
2016-09-27 17:58:47 -0500

    initial commit with working hello world

---
+++ js/hello-world.js
@@ -0,0 +1,9 @@
+var App = React.createClass({
+  render: function(){
+    return <div>
+      Hello, from Shawn!
+    </div>;
+  }
+});
+
+React.render(React.createElement(App), document.getElementById('root'));
