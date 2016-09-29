4257a02b2bf27999b73ac53fbf5ef896c5ad3874
Ram Iyer <ramky.iyer@gmail.com>
2016-09-29 16:39:24 -0500

    creating reusable components and using bootstrap tables

--- js/open-library.js
+++ js/open-library.js
@@ -1,3 +1,48 @@
+var Heading = React.createClass({
+  render: function() {
+    return <th>{this.props.heading}</th>
+  }
+});
+
+var Headings = React.createClass({
+  render: function() {
+    var headings = this.props.headings.map(function(name, i) {
+      return <Heading key={i} heading={name} />;
+    });
+    return <thead><tr>{headings}</tr></thead>;
+  }
+});
+
+var Row = React.createClass({
+  render: function() {
+    return <tr>
+             <td>{this.props.changeSet.when}</td>
+             <td>{this.props.changeSet.who}</td>
+             <td>{this.props.changeSet.description}</td>
+           </tr>;
+  }
+});
+
+var Rows = React.createClass({
+  render: function() {
+    var rows = this.props.changeSets.map(function(changeSet, i) {
+      return(<Row key={i} changeSet = {changeSet}/>);
+    });
+    return <tbody>{rows}</tbody>;
+  }
+});
+
+
+var App = React.createClass({
+  render: function() {
+    return <table className = 'table'>
+             <Headings headings = {this.props.headings} />
+             <Rows changeSets = {this.props.changeSets} />
+           </table>;
+    }
+});
+
+var headings = ['When', 'Who', 'Description'];
 var data = [
   {
     "when": "2 minutes ago",
@@ -16,42 +61,6 @@ var data = [
   }
 ];
 
-var App = React.createClass({
-  render: function(){
-    var headings = this.props.headings.map(function(heading, i) {
-      return(
-          <th key={i}>
-            {heading}
-          </th>
-      );
-    });
+ReactDOM.render(<App headings = {headings} changeSets = {data} />,
+  document.getElementById('app'));
 
-    var rows = this.props.data.map(function(row, i){
-      return(
-        <tr key={i}>
-          <td> {row.when} </td>
-          <td> {row.who} </td>
-          <td> {row.description} </td>
-        </tr>
-      );
-    });
-
-    return(
-      <div>
-        <h1>{this.props.title}</h1>
-        <table>
-          <tbody>
-            <tr>
-              {headings}
-            </tr>
-            {rows}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-});
-
-var headings = [ 'Last updated at', 'By Author', 'Summary' ]
-var title = "Recent changes";
-ReactDOM.render(<App headings={headings} data={data} title={title} />, document.getElementById('app'));
